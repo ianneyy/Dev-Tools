a7becb63ba7530f262d8f07866ed62c9aa058014
ianneyy <jimmygomez1230@gmail.com>
2025-06-18 00:51:48 +0800

    fix cta translate-y

--- src/components/ui/bento-grid.tsx
+++ src/components/ui/bento-grid.tsx
@@ -48,11 +48,11 @@ const BentoCard = ({
       "bg-white [box-shadow:0_0_0_1px_rgba(0,0,0,.03),0_2px_4px_rgba(0,0,0,.05),0_12px_24px_rgba(0,0,0,.05)]",
       // dark styles
       "transform-gpu dark:bg-black dark:[border:1px_solid_rgba(255,255,255,.1)] dark:[box-shadow:0_-20px_80px_-20px_#ffffff1f_inset]",
-      className,
+      className
     )}
   >
     <div>{background}</div>
-    <div className="pointer-events-none z-10 flex transform-gpu flex-col gap-1 p-6 transition-all duration-300 group-hover:-translate-y-10">
+    <div className="pointer-events-none z-10 flex transform-gpu flex-col gap-1 p-6 transition-all duration-300 -translate-y-10   lg:translate-y-0 lg:group-hover:-translate-y-10">
       {/* <Icon className="h-12 w-12 origin-left transform-gpu text-neutral-700 transition-all duration-300 ease-in-out group-hover:scale-75" /> */}
       <h3 className="text-xl font-semibold text-neutral-700 dark:text-neutral-300">
         {name}
@@ -62,7 +62,10 @@ const BentoCard = ({
 
     <div
       className={cn(
-        "pointer-events-none absolute bottom-0 flex w-full translate-y-10 transform-gpu flex-row items-center p-4 opacity-0 transition-all duration-300 group-hover:translate-y-0 group-hover:opacity-100",
+        "absolute bottom-0 flex w-full transform-gpu flex-row items-center p-4 transition-all duration-300",
+        "opacity-100 pointer-events-auto", // default for mobile
+        "lg:translate-y-10 lg:opacity-0 lg:pointer-events-none", // hide by default on lg
+        "lg:group-hover:translate-y-0 lg:group-hover:opacity-100 lg:group-hover:pointer-events-auto"
       )}
     >
       <Button variant="ghost" asChild size="sm" className="pointer-events-auto">
